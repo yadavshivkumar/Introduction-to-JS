cab3d1b30014c7f319f2a2f6a19468cc278f3b7b
Shiv Kumar Yadav <y.shiv@op.iitg.ac.in>
2026-07-17 06:50:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Hello, World! This is a simple JavaScript file.")
+
+console.log("You can add more functionality to this file as needed.")
+
+console.log("Feel free to experiment with JavaScript and see how it works!")    
+
+console.log("Remember to open the console in your browser to see the output of this script.")   
+
+console.log("Happy coding!")
+
+console.log("This script is a great starting point for learning JavaScript. You can add more code to it as you learn new concepts and features of the language.")   
+
+console.log("Don't forget to save your changes and refresh the page to see the updated output in the console.")
+
+console.log("Keep practicing and exploring JavaScript to become proficient in web development!")
+
+console.log("You can also use this script to test out new JavaScript features or to debug your code by adding console.log statements to see the values of variables at different points in your program.")
+
+console.log("As you continue to learn JavaScript, you can expand this script to include functions, loops, conditionals, and more complex logic to create interactive web applications.")
+
+console.log("The possibilities with JavaScript are endless, so keep experimenting and have fun coding!")    
